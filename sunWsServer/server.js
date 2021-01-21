9f1b6b4d641121089a8f2869c93b01cb7167955e
Xiangwen Sun <sun0918tuc@gmail.com>
2021-01-21 14:24:56 +0100

    sending data

--- sunWsServer/server.js
+++ sunWsServer/server.js
@@ -13,17 +13,34 @@ const sktIo = socketio(server, {
 });
 
 const PORT = 3000;
+const UPDATE_TREE_ROOT = 'updatetree01Root';
+const UPDATE_TREE_CHILDREN = 'updatetree01Children';
+const TREE_ROOT = 'tree01 root';
+const TREE_CHILDREN = 'tree01 children';
+const WS_READY='wsReady';
 
 sktIo.on('connection', (socket) => {
     console.log('new webSocket connection...');
-    socket.emit('eventName01', 'hello from ws server');
+    socket.emit('wsReady', 'hello from ws server');
 
     // handle the event
-    socket.on('updatetree', updateTreeListener);
+    socket.on(UPDATE_TREE_ROOT, updateTreeListenerRoot);
+    socket.on(UPDATE_TREE_CHILDREN, updateTreeListenerChildren);
 });
 
-const updateTreeListener = (data) => {
+const updateTreeListenerRoot = (data) => {
     console.log(data);
+    sktIo.emit(TREE_ROOT, {'root': ['programming', 'life and study']});
+  }
+
+  const updateTreeListenerChildren = (data) => {
+    console.log(data);
+    sktIo.emit(TREE_CHILDREN, {'children': [
+      ['life and study', ['language', 'study in Germany', 'travel']],
+      ['programming', ['java', 'c++', 'database']],
+      ['language', ['Chinese', 'German']],
+      ['java', ['java architecture', 'java concepts', 'jvm', 'garbage collection', 'java performance tuning', 'spring']],
+    ]});
   }
 
 server.listen(PORT, () => console.log('WebSocket Server running on port ' + PORT));
